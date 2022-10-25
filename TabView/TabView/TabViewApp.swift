befcc4255ed4f69714e36cc4d0b584ccf2a80faa
Ilona Sellenberk <ilona.sell@gmail.com>
2022-10-25 13:29:41 +0200

    initial commit app files

---
+++ TabView/TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Ilona Sellenberkova on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
